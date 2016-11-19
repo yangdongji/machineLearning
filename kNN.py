b04abecd372fb48ab2402f09eedead82ac8f54b2
Alex Young <12418339@qq.com>
2016-11-19 12:11:18 +0800

    Update kNN.py

--- kNN.py
+++ kNN.py
@@ -1,78 +1,75 @@
-'''
-Created on 9.19 2016
-kNN: k Nearest Neighbors
-
-Input:      inX: vector to compare to existing dataset (1xN)
-            dataSet: size m data set of known vectors (NxM)
-            labels: data set labels (1xM vector)
-            k: number of neighbors to use for comparison (should be an odd number)
-            
-Output:     the most popular class label
-
-@author: alex young
-'''
 from numpy import *
 import operator
 from os import listdir
 
-def classify0(inX, dataSet, labels, k):
+def createDataSet():
+    group = array([[1.0,1.1],[1.0,1.0],[0,0],[0,0.1]])
+    labels =['A','A','B','B']
+    return group , labels
+
+def classify0(inX,dataSet,labels,k):
     dataSetSize = dataSet.shape[0]
-    diffMat = tile(inX, (dataSetSize,1)) - dataSet
+    diffMat = tile(inX , (dataSetSize,1)) - dataSet
     sqDiffMat = diffMat**2
-    sqDistances = sqDiffMat.sum(axis=1)
+    sqDistances = sqDiffMat.sum(axis = 1)
     distances = sqDistances**0.5
-    sortedDistIndicies = distances.argsort()     
-    classCount={}          
+    sortedDistIndicies = distances.argsort()
+    classCount ={}
     for i in range(k):
         voteIlabel = labels[sortedDistIndicies[i]]
         classCount[voteIlabel] = classCount.get(voteIlabel,0) + 1
-    sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), reverse=True)
-    return sortedClassCount[0][0]
-
-def createDataSet():
-    group = array([[1.0,1.1],[1.0,1.0],[0,0],[0,0.1]])
-    labels = ['A','A','B','B']
-    return group, labels
+    sortedClassCount = sorted(classCount.iteritems(),key  = operator.itemgetter(1) , reverse = True)
+    return sortedClassCount[0][0] 
 
 def file2matrix(filename):
     fr = open(filename)
-    numberOfLines = len(fr.readlines())         #get the number of lines in the file
-    returnMat = zeros((numberOfLines,3))        #prepare matrix to return
-    classLabelVector = []                       #prepare labels return   
-    fr = open(filename)
+    arrayOlines = fr.readlines()
+    numberOfLines = len(arrayOlines)
+    returnMat = zeros((numberOfLines,3))
+    classLabelVector = []
     index = 0
-    for line in fr.readlines():
+    for line in arrayOlines:
         line = line.strip()
         listFromLine = line.split('\t')
         returnMat[index,:] = listFromLine[0:3]
         classLabelVector.append(int(listFromLine[-1]))
-        index += 1
+        index+=1
     return returnMat,classLabelVector
-    
+
 def autoNorm(dataSet):
     minVals = dataSet.min(0)
     maxVals = dataSet.max(0)
     ranges = maxVals - minVals
     normDataSet = zeros(shape(dataSet))
-    m = dataSet.shape[0]
-    normDataSet = dataSet - tile(minVals, (m,1))
-    normDataSet = normDataSet/tile(ranges, (m,1))   #element wise divide
-    return normDataSet, ranges, minVals
-   
+    m  = dataSet.shape[0]
+    normDataSet = dataSet - tile(minVals,(m,1))
+    normDataSet = normDataSet/tile(ranges,(m,1))
+    return normDataSet,ranges,minVals
+
 def datingClassTest():
-    hoRatio = 0.50      #hold out 10%
-    datingDataMat,datingLabels = file2matrix('datingTestSet2.txt')       #load data setfrom file
-    normMat, ranges, minVals = autoNorm(datingDataMat)
+    hoRatio =0.10
+    datingDataMat , datingLabels = file2matrix('datingTestSet2.txt')
+    normMat ,ranges ,minVals = autoNorm(datingDataMat)
     m = normMat.shape[0]
     numTestVecs = int(m*hoRatio)
     errorCount = 0.0
     for i in range(numTestVecs):
         classifierResult = classify0(normMat[i,:],normMat[numTestVecs:m,:],datingLabels[numTestVecs:m],3)
-        print "the classifier came back with: %d, the real answer is: %d" % (classifierResult, datingLabels[i])
+        print "the classifier came back with:%d,the real answer is: %d" %(classifierResult,datingLabels[i])
         if (classifierResult != datingLabels[i]): errorCount += 1.0
-    print "the total error rate is: %f" % (errorCount/float(numTestVecs))
-    print errorCount
-    
+    print    "the total error rate is %f "%(errorCount/float(numTestVecs))
+
+def classifyPerson():
+    resultList = ['not at all','is small doses','in large doses']
+    percentTats = float(raw_input("percentage of time spent playing video games?"))
+    ffMiles = float(raw_input("frequent flier miles earned per year?"))
+    iceCream = float(raw_input("liters of ice cream consumed per year?"))
+    datingDataMat , datingLabels = file2matrix('datingTestSet2.txt')
+    normMat , ranges , minVals = autoNorm(datingDataMat)
+    inArr = array([ffMiles,percentTats,iceCream])
+    classifierResult = classify0((inArr-minVals)/ranges,normMat,datingLabels,3)
+    print "You will probably like this person:",resultList[classifierResult-  1]
+
 def img2vector(filename):
     returnVect = zeros((1,1024))
     fr = open(filename)
